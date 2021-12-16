ec1b03fcc4f919ffc2acd37657207e3bf3a1f42d
houdini217 <koya2170@gmail.com>
2021-12-17 00:35:47 +0900

    Transfer Assembler to https://github.com/KoyaYamasaki/Assembler

--- 06/Assembler/CommandType.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum CommandType: String {
-    case A_COMMAND = "A_COMMAND"
-    case C_COMMAND = "C_COMMAND"
-    case L_COMMAND = "L_COMMAND"
-    case UNKNOWN = "UNKNOWN"
-}
